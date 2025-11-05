c7271758857f32465fb56745c07da37e3cd75ffe
Benjamin <omacknight@gmail.com>
2025-11-05 14:38:06 +0000

    Final
    Finished (100%)

--- game.js
+++ game.js
@@ -24,7 +24,7 @@ let score = 0;
 function drawScore() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
-ctx.fillText(`Score: ${score}`, 8, 20);
+    ctx.fillText(`Score: ${score}`, 8, 20);
 }
 
 
